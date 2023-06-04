f4fa3dccceb2f64acc0c9fe3da32326899eda3d1
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-06-04 17:29:29 +0300

    update models

---
+++ models/gaming_laptop.py
@@ -0,0 +1,49 @@
+"""
+This module defines a GamingLaptop class that represents
+a laptop specifically designed for gamers, inheriting from the Laptop class.
+"""
+
+# pylint: disable= import-error
+from models.abstract_laptop import AbstractLaptop
+
+
+# pylint: disable= too-few-public-methods
+class GamingLaptop(AbstractLaptop):
+    """
+    Represents a gaming laptop, inheriting from the AbstractLaptop class.
+    """
+
+    # pylint: disable= too-many-arguments
+    def __init__(self, model="Asus", screen_size=17.3, ram=32,
+                 storage=1024, battery_life=8, battery_level=100,
+                 gpu="NVIDIA GeForce RTX 3080", fan_count=2):
+        """
+        Initializes the GamingLaptop object.
+
+        Args:
+            model (str): The model of the laptop. Defaults to "Asus".
+            screen_size (float): The screen size in inches. Defaults to 17.3.
+            ram (int): The RAM size in gigabytes. Defaults to 32.
+            storage (int): The storage capacity in gigabytes. Defaults to 1024.
+            battery_life (int): The battery life in hours. Defaults to 8.
+            battery_level (int): The current battery level in percentage. Defaults to 100.
+            gpu (str): The GPU model of the laptop. Defaults to "NVIDIA GeForce RTX 3080".
+            fan_count (int): The number of fans in the laptop. Defaults to 2.
+        """
+        super().__init__(model, screen_size, ram, storage, battery_life, battery_level)
+        self.gpu = gpu
+        self.fan_count = fan_count
+        self.optimal_processor_temperature = {65, 80}
+
+    def __str__(self):
+        """
+        Returns a string representation of the gaming laptop.
+
+        Returns:
+            str: The string representation of the gaming laptop.
+        """
+        return f'Model: {self.model}, Screen size: {self.screen_size},' \
+               f' Ram: {self.ram}, Storage: {self.storage}, ' \
+               f'Battery life: {self.battery_life}, Battery level: {self.battery_level},' \
+               f' GPU: {self.gpu}, ' \
+               f'Fan count: {self.fan_count}'
---
+++ models/office_laptop.py
@@ -0,0 +1,48 @@
+"""
+This module defines a OfficeLaptop class that represents
+a laptop specifically designed for office, inheriting from the Laptop class.
+"""
+
+# pylint: disable= import-error
+from models.abstract_laptop import AbstractLaptop
+
+
+# pylint: disable= too-few-public-methods
+class OfficeLaptop(AbstractLaptop):
+    """
+    Represents an office laptop, inheriting from the AbstractLaptop class.
+    """
+    # pylint: disable= too-many-arguments
+    def __init__(self, model="Lenovo", screen_size=17.3,
+                 ram=16, storage=256, battery_life=6, battery_level=100,
+                 color="Black", price=25000):
+        """
+        Initializes the OfficeLaptop object.
+
+        Args:
+            model (str): The model of the office laptop. Defaults to "Lenovo".
+            screen_size (float): The screen size in inches. Defaults to 17.3.
+            ram (int): The RAM size in gigabytes. Defaults to 16.
+            storage (int): The storage capacity in gigabytes. Defaults to 256.
+            battery_life (int): The battery life in hours. Defaults to 6.
+            battery_level (int): The current battery level in percentage. Defaults to 100.
+            color (str): The color of the laptop. Defaults to "Black".
+            price (int): The price of the laptop. Defaults to 25000.
+        """
+        super().__init__(model, screen_size, ram, storage, battery_life, battery_level)
+        self.color = color
+        self.price = price
+        self.optimal_processor_temperature = {45, 60}
+
+    def __str__(self):
+        """
+        Returns a string representation of the office laptop.
+
+        Returns:
+            str: The string representation of the office laptop.
+        """
+        return f'Model: {self.model}, Screen size: {self.screen_size},' \
+               f' Ram: {self.ram}, Storage: {self.storage}, ' \
+               f'Battery life: {self.battery_life}, Battery level: {self.battery_level},' \
+               f' Color: {self.color}, ' \
+               f'Price: {self.price}'
